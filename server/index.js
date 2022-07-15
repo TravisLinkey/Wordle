16d2a4cb4cc8054b648512443989192b5413ae24
liftskit <liftskit@gmail.com>
2022-07-14 17:54:01 -0700

    added random word route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/getWord", (req, res) => {
+  try {
+    const words = fs.readFileSync("./words.csv", {
+      encoding: "utf8",
+      flag: "r",
+    });
+    // save words to array
+    const wordsArray = words.replaceAll(",", "").split("\n");
+
+    // get random indexed word
+    const index = Math.floor(Math.random() * 100);
+
+    res.send(wordsArray[index]);
+  } catch (error) {
+    res.error("Error: ", error);
+  }
 });
 
 app.listen(PORT, () => {
